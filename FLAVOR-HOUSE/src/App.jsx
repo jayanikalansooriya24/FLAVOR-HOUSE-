9aead349effd644f30a73feaa96bb56d3f00c788
Peshala G A K J <jayanikalansooriya24@gmail.com>
2026-01-22 10:25:24 +0530

    feat: add OneFood component with styling and functionality for food selection, quantity management, and cart integration

--- FLAVOR-HOUSE/src/App.jsx
+++ FLAVOR-HOUSE/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Getstart from "./Pages/Getstart";
 import Signin from "./Pages/Signin";
 import Home from "./Pages/Home";
@@ -8,7 +9,7 @@ import Desserts from "./Pages/Desserts";
 import Beverages from "./Pages/Beverages";
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
-
+import OneFood from "./Pages/Product/OneFood"; // ✅ FIXED
 
 function App() {
   return (
@@ -17,15 +18,15 @@ function App() {
         <Route path="/" element={<Getstart />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/Appetizers" element={<Appetizers />} />
-        <Route path="/MainCourse" element={<MainCourse />} />
-        <Route path="/Desserts" element={<Desserts />} />
-        <Route path="/Beverages" element={<Beverages />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/appetizers" element={<Appetizers />} />
+        <Route path="/maincourse" element={<MainCourse />} />
+        <Route path="/desserts" element={<Desserts />} />
+        <Route path="/beverages" element={<Beverages />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
 
-      
-        
+        {/* OneFood route */}
+        <Route path="/onefood" element={<OneFood />} />
       </Routes>
     </Router>
   );
